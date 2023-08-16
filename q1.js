f45bf28ce434f30dc371dad2a855f8f5b9c165e7
gabrdiias <127976037+gabrdiias@users.noreply.github.com>
2023-08-16 15:39:56 -0300

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,53 @@
+
+const resultado = document.getElementById("resultado")
+
+function verificaResposta(resposta){
+    if (resposta === "b"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } else{
+        resultado.innerHTML = "Resposta incorreta";
+        resultado.style.color = "red"
+    }
+}
+
+function verificaResposta2(resposta){
+    if (resposta === "d"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } else{
+        resultado.innerHTML = "Resposta incorreta";
+        resultado.stylcolor = "red"
+    }
+}
+
+function verificaResposta3(resposta){
+    if (resposta === "b"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } else{
+        resultado.innerHTML = "Resposta incorreta";
+        resultado.stylcolor = "red"
+    }
+}
+
+function verificaResposta4(resposta){
+    if (resposta === "a"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } else{
+        resultado.innerHTML = "Resposta incorreta";
+        resultado.stylcolor = "red"
+    }
+}   
+
+function verificaResposta5(resposta){
+    if (resposta === "c"){
+        resultado.innerHTML = "Resposta correta";
+        resultado.style.color = "green"
+    } else{
+        resultado.innerHTML = "Resposta incorreta";
+        resultado.stylcolor = "red"
+    }
+}
+
